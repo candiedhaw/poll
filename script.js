78ed6ea9ad9f49f2c0269bd557fd4ebad8bf0e8e
candiedhaw <84980393+candiedhaw@users.noreply.github.com>
2023-12-14 00:44:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,54 @@
-const daysOfWeek = ["Dec 31, Sun", "Jan 1, Mon", "Jan 2, Tue", "Jan 3, Wed", "Jan 4, Thu", "Jan 5, Fri", "Jan 6, Sat", "Jan 7, Sun"];
-const timeSlots = ["8:00-8:30", "8:30-9:00", "9:00-9:30", "9:30-10:00"];
+let dates = ["Dec 31, Sun", "Jan 1, Mon", "Jan 2, Tue", "Jan 3, Wed", "Jan 4, Thu", "Jan 5, Fri", "Jan 6, Sat", "Jan 7, Sun"];
+let times = ["8:00", "8:30", "9:00", "9:30", "10:00"];
+let votes = {};
 
-function createVotingTable() {
-    const votingTable = document.getElementById("votingTable");
-
-    // Create the header row with date titles
-    const headerRow = document.createElement("tr");
-    headerRow.appendChild(document.createElement("th")); // Empty cell for the first column
-
-    daysOfWeek.forEach(day => {
-        const headerCell = document.createElement("th");
-        headerCell.textContent = day;
-        headerRow.appendChild(headerCell);
-    });
-
-    votingTable.appendChild(headerRow);
-
-    // Create rows for each time slot
-    timeSlots.forEach((timeSlot, rowIndex) => {
-        const row = document.createElement("tr");
-
-        // Add the time slot to the first column
-        const timeSlotCell = document.createElement("td");
-        timeSlotCell.textContent = timeSlot;
-        row.appendChild(timeSlotCell);
-
-        // Add checkboxes for each day in columns [2 to 9]
-        daysOfWeek.forEach((day, colIndex) => {
-            const checkBoxCell = document.createElement("td");
-            const checkBox = document.createElement("input");
-            checkBox.type = "checkbox";
-            checkBox.classList.add("check-box");
-            checkBox.id = `checkbox-${rowIndex}-${colIndex}`;
-            checkBoxCell.appendChild(checkBox);
-            row.appendChild(checkBoxCell);
-        });
-
-        votingTable.appendChild(row);
-    });
+function createTable() {
+    let table = document.getElementById('pollTable');
+    let headerRow = document.createElement('tr');
+    let nameHeader = document.createElement('th');
+    nameHeader.innerHTML = "Name/Time";
+    headerRow.appendChild(nameHeader);
+    for (let date of dates) {
+        let dateHeader = document.createElement('th');
+        dateHeader.innerHTML = date;
+        headerRow.appendChild(dateHeader);
+    }
+    table.appendChild(headerRow);
+    for (let time of times) {
+        let row = document.createElement('tr');
+        let timeCell = document.createElement('td');
+        timeCell.innerHTML = time;
+        row.appendChild(timeCell);
+        for (let date of dates) {
+            let cell = document.createElement('td');
+            let checkbox = document.createElement('input');
+            checkbox.type = "checkbox";
+            checkbox.name = "vote";
+            checkbox.value = date + " " + time;
+            cell.appendChild(checkbox);
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    }
 }
 
-function submitVote() {
-    const nameInput = document.getElementById("name").value;
-    const selectedOptions = [];
-
-    // Loop through checkboxes to check which ones are selected
-    timeSlots.forEach((timeSlot, rowIndex) => {
-        daysOfWeek.forEach((day, colIndex) => {
-            const checkBox = document.getElementById(`checkbox-${rowIndex}-${colIndex}`);
-            if (checkBox.checked) {
-                selectedOptions.push({ name: nameInput, day, time: timeSlot });
+function getResults() {
+    let checkboxes = document.getElementsByName('vote');
+    for (let checkbox of checkboxes) {
+        if (checkbox.checked) {
+            if (votes[checkbox.value]) {
+                votes[checkbox.value]++;
+            } else {
+                votes[checkbox.value] = 1;
             }
-        });
-    });
-
-    // Display the selected options
-    console.log(`${nameInput} voted for:`, selectedOptions);
-
-    // You can further handle the submission logic (e.g., send data to a server or store in a database)
+        }
+    }
+    let results = Object.entries(votes).sort((a, b) => b[1] - a[1]).slice(0, 3);
+    let resultsDiv = document.getElementById('results');
+    resultsDiv.innerHTML = "";
+    for (let result of results) {
+        resultsDiv.innerHTML += result[0] + ": " + result[1] + " votes<br>";
+    }
 }
 
-// Call the function to create the voting table when the page loads
-createVotingTable();
+createTable();
