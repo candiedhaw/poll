d72dbfa2ee4a2186f2fb031076ebaef53c4fd967
candiedhaw <84980393+candiedhaw@users.noreply.github.com>
2023-12-16 23:31:32 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,86 @@
-let dates = ["Dec 31, Sun", "Jan 1, Mon", "Jan 2, Tue", "Jan 3, Wed", "Jan 4, Thu", "Jan 5, Fri", "Jan 6, Sat", "Jan 7, Sun"];
-let times = ["8:00", "8:30", "9:00", "9:30", "10:00"];
-let votes = {};
+const pollTitle = "PAG Prep Schedule Poll";
+const timeZone = "Central Time";
+const meetingStartTime = { weekdays: "8:00 PM", weekends: "9:00 AM" };
+const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const dateRange = ["Dec 31, Sun", "Jan 1, Mon", "Jan 2, Tue", "Jan 3, Wed", "Jan 4, Thu", "Jan 5, Fri", "Jan 6, Sat", "Jan 7, Sun"];
 
-function createTable() {
-    let table = document.getElementById('pollTable');
-    let headerRow = document.createElement('tr');
-    let nameHeader = document.createElement('th');
-    nameHeader.innerHTML = "Name/Time";
-    headerRow.appendChild(nameHeader);
-    for (let date of dates) {
-        let dateHeader = document.createElement('th');
-        dateHeader.innerHTML = date;
-        headerRow.appendChild(dateHeader);
-    }
-    table.appendChild(headerRow);
-    for (let time of times) {
-        let row = document.createElement('tr');
-        let timeCell = document.createElement('td');
-        timeCell.innerHTML = time;
-        row.appendChild(timeCell);
-        for (let date of dates) {
-            let cell = document.createElement('td');
-            let checkbox = document.createElement('input');
-            checkbox.type = "checkbox";
-            checkbox.name = "vote";
-            checkbox.value = date + " " + time;
-            cell.appendChild(checkbox);
-            row.appendChild(cell);
+const container = document.querySelector('.container');
+const scheduleTable = document.getElementById('scheduleTable');
+const nameInput = document.getElementById('nameInput');
+const votingResults = document.getElementById('votingResults');
+
+function createSchedulePoll() {
+    // Clear previous content
+    scheduleTable.innerHTML = '';
+
+    // Create header row with dates
+    const headerRow = scheduleTable.insertRow(0);
+    headerRow.insertCell(0); // Empty cell for names
+    dateRange.forEach((date, index) => {
+        const cell = headerRow.insertCell(index + 1);
+        cell.textContent = date;
+    });
+
+    // Create rows for time slots
+    for (let hour = 1; hour <= 23; hour++) {
+        const timeSlotRow = scheduleTable.insertRow(hour);
+        const timeSlotCell = timeSlotRow.insertCell(0);
+        timeSlotCell.textContent = `${hour}:00 - ${hour + 1}:00`;
+
+        // Create checkboxes for each day
+        for (let dayIndex = 0; dayIndex < dateRange.length; dayIndex++) {
+            const checkBoxCell = timeSlotRow.insertCell(dayIndex + 1);
+            const checkBox = document.createElement("input");
+            checkBox.type = "checkbox";
+            checkBox.classList.add("check-box");
+            checkBoxCell.appendChild(checkBox);
         }
-        table.appendChild(row);
     }
 }
 
-function getResults() {
-    let checkboxes = document.getElementsByName('vote');
-    for (let checkbox of checkboxes) {
-        if (checkbox.checked) {
-            if (votes[checkbox.value]) {
-                votes[checkbox.value]++;
-            } else {
-                votes[checkbox.value] = 1;
+function submitVote() {
+    const voterName = nameInput.value;
+    const selectedOptions = [];
+
+    // Loop through checkboxes to check which ones are selected
+    for (let hour = 1; hour <= 23; hour++) {
+        for (let dayIndex = 0; dayIndex < dateRange.length; dayIndex++) {
+            const checkBox = scheduleTable.rows[hour].cells[dayIndex + 1].querySelector("input");
+            if (checkBox.checked) {
+                const selectedDate = dateRange[dayIndex];
+                const selectedTime = `${hour}:00 - ${hour + 1}:00`;
+                selectedOptions.push({ voterName, selectedDate, selectedTime });
             }
         }
     }
-    let results = Object.entries(votes).sort((a, b) => b[1] - a[1]).slice(0, 3);
-    let resultsDiv = document.getElementById('results');
-    resultsDiv.innerHTML = "";
-    for (let result of results) {
-        resultsDiv.innerHTML += result[0] + ": " + result[1] + " votes<br>";
+
+    // Display the selected options
+    displayResults(selectedOptions);
+}
+
+function displayResults(selectedOptions) {
+    // Clear previous results
+    votingResults.innerHTML = '';
+
+    // Display the results
+    if (selectedOptions.length > 0) {
+        const resultsHeading = document.createElement('h2');
+        resultsHeading.textContent = "Voting Results";
+        votingResults.appendChild(resultsHeading);
+
+        const resultsList = document.createElement('ul');
+        selectedOptions.forEach(option => {
+            const listItem = document.createElement('li');
+            listItem.textContent = `${option.voterName} voted on ${option.selectedDate} at ${option.selectedTime}`;
+            resultsList.appendChild(listItem);
+        });
+        votingResults.appendChild(resultsList);
+    } else {
+        const noVotesMessage = document.createElement('p');
+        noVotesMessage.textContent = "No votes recorded yet.";
+        votingResults.appendChild(noVotesMessage);
     }
 }
 
-createTable();
+// Call the function to create the schedule poll when the page loads
+createSchedulePoll();
